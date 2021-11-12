0de4fb32b6c1e53184d50561cc46dd09c6dde3a8
Eugene Kiryk <kiryk.eugene@gmail.com>
2021-11-12 12:17:08 +0100

    assets.js added

---
+++ src/assets.js
@@ -0,0 +1,31 @@
+import IconCart from './assets/icon-cart.svg';
+import IconClose from './assets/icon-close.svg';
+import IconDelete from './assets/icon-delete.svg';
+import IconMenu from './assets/icon-menu.svg';
+import IconPlus from './assets/icon-plus.svg';
+import IconMinus from './assets/icon-minus.svg';
+import IconNext from './assets/icon-next.svg';
+import IconPrevious from './assets/icon-previous.svg';
+
+import Image1 from './assets/image-product-1.jpg';
+import Image2 from './assets/image-product-2.jpg';
+import Image3 from './assets/image-product-3.jpg';
+import Image4 from './assets/image-product-4.jpg';
+
+export const images = [ 
+  { id: 'img1', src: Image1 }, 
+  { id: 'img2', src: Image2 }, 
+  { id: 'img3', src: Image3 },
+  { id: 'img4', src: Image4 }
+];
+
+export const icons = {
+  IconCart,
+  IconClose,
+  IconDelete,
+  IconMenu,
+  IconNext,
+  IconPrevious,
+  IconPlus,
+  IconMinus
+};
