452c4a70bf20c84d54a403311561188c702cf180
Eugene Kiryk <kiryk.eugene@gmail.com>
2021-11-13 22:48:13 +0100

    format code

--- src/components/Layout/Navigation/Navigation.js
+++ src/components/Layout/Navigation/Navigation.js
@@ -13,72 +13,72 @@ import NavLinks from './NavLinks';
 import classes from './Navigation.module.css';
 
 function Navigation() {
-	const ctx = useContext(AppContext);
+  const ctx = useContext(AppContext);
 
-	const [showDrawer, setShowDrawer] = useState(false);
-	const [showCart, setShowCart] = useState(false);
-	
-	const mobileNav = ctx.windowWidth <= 1024;
+  const [showDrawer, setShowDrawer] = useState(false);
+  const [showCart, setShowCart] = useState(false);
 
-	const showDrawerHandler = () => {
-		setShowDrawer(true);
-	}
+  const mobileNav = ctx.windowWidth <= 1024;
 
-	const hideDrawerHandler = () => {
-		setShowDrawer(false);
-	}
+  const showDrawerHandler = () => {
+    setShowDrawer(true);
+  }
 
-	const toggleCartHandler = () => {
-		setShowCart(!showCart);
-	}
+  const hideDrawerHandler = () => {
+    setShowDrawer(false);
+  }
 
-	return (
-		<header className={classes.header}>
-			{
-				showDrawer && mobileNav ? 
-					<Drawer onHideDrawer={hideDrawerHandler} /> : null
-			}
-			<div className={classes['header-wrapper']}>
-				<div className={classes['nav-links']}>
-					{
-						mobileNav ? 
-						<div className={classes['hamburger-menu']}>
-							<img src={MenuIcon} alt="menu" onClick={showDrawerHandler} />
-						</div> : null
-					}
-					<div className={classes.logo}>
-						<img src={Logo} alt="sneakers" />
-					</div>
-					{
-						!mobileNav ? 
-						<nav className={classes.nav}>
-							<NavLinks />
-						</nav> : null 
-					}
-				</div>
-				<div className={classes['nav-icons']}>
-					<div className={classes['cart-icon']}>
-						{
-							ctx.cartQuantity !== 0 && 
-							<p className={classes['cart-quantity']}>{ctx.cartQuantity}</p>
-						}
-						<img src={CartIcon} alt="cart" onClick={toggleCartHandler} />
-					</div>
-					<div className={classes.avatar}>
-						<img src={Avatar} alt="avatar" />
-					</div>
-				</div>
-			</div>
-			{
-				showCart ? (
-					<Fragment>
-						<Backdrop transparent onClick={toggleCartHandler} />
-						<Cart />
-					</Fragment>
-				) : null
-			}
-		</header>
-	);
+  const toggleCartHandler = () => {
+    setShowCart(!showCart);
+  }
+
+  return (
+    <header className={classes.header}>
+      {
+        showDrawer && mobileNav ?
+          <Drawer onHideDrawer={hideDrawerHandler} /> : null
+      }
+      <div className={classes['header-wrapper']}>
+        <div className={classes['nav-links']}>
+          {
+            mobileNav ?
+              <div className={classes['hamburger-menu']}>
+                <img src={MenuIcon} alt="menu" onClick={showDrawerHandler} />
+              </div> : null
+          }
+          <div className={classes.logo}>
+            <img src={Logo} alt="sneakers" />
+          </div>
+          {
+            !mobileNav ?
+              <nav className={classes.nav}>
+                <NavLinks />
+              </nav> : null
+          }
+        </div>
+        <div className={classes['nav-icons']}>
+          <div className={classes['cart-icon']}>
+            {
+              ctx.cartQuantity !== 0 &&
+              <p className={classes['cart-quantity']}>{ctx.cartQuantity}</p>
+            }
+            <img src={CartIcon} alt="cart" onClick={toggleCartHandler} />
+          </div>
+          <div className={classes.avatar}>
+            <img src={Avatar} alt="avatar" />
+          </div>
+        </div>
+      </div>
+      {
+        showCart ? (
+          <Fragment>
+            <Backdrop transparent onClick={toggleCartHandler} />
+            <Cart />
+          </Fragment>
+        ) : null
+      }
+    </header>
+  );
 }
 
 export default Navigation;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -13,160 +13,160 @@ import classes from './Product.module.css';
 const getCurrentImage = (e, images) => images.find(image => image.id === e.target.id);
 
 function Product() {
-	const ctx = useContext(AppContext);
-	const mobile = ctx.windowWidth <= 1024;
+  const ctx = useContext(AppContext);
+  const mobile = ctx.windowWidth <= 1024;
 
-	const [showModal, setShowModal] = useState(false);
-	const [currentImage, setCurrentImage] = useState(images[0]);
-	const [productQuantity, setProductQuantity] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [currentImage, setCurrentImage] = useState(images[0]);
+  const [productQuantity, setProductQuantity] = useState(0);
 
-	const productInfo = {
-		id: 'product1',
-		title: 'Fall Limited Edition Sneakers',
-		price: 125,
-		description: `
+  const productInfo = {
+    id: 'product1',
+    title: 'Fall Limited Edition Sneakers',
+    price: 125,
+    description: `
 			These low-profile sneakers are your perfect casual wear companion. 
 			Featuring a durable rubber outer sole, 
 			they'll withstand everything the weather can offer.
 		`,
-		image: images[0].src
-	};
+    image: images[0].src
+  };
 
-	const openImageHandler = (e) => {
-		const image = getCurrentImage(e, images);
-		setCurrentImage(image);
-	}
+  const openImageHandler = (e) => {
+    const image = getCurrentImage(e, images);
+    setCurrentImage(image);
+  }
 
-	const changeImageHandler = (idx) => {
-		const currentIdx = images.findIndex(image => image.id === currentImage.id);
-		const sumIdx = currentIdx + idx;
-		const newImage = images[sumIdx];
+  const changeImageHandler = (idx) => {
+    const currentIdx = images.findIndex(image => image.id === currentImage.id);
+    const sumIdx = currentIdx + idx;
+    const newImage = images[sumIdx];
 
-		if (sumIdx >= images.length) {
-			setCurrentImage(images[0]);
-		} else if (sumIdx < 0) {
-			setCurrentImage(images[images.length - 1]);
-		} else {
-			setCurrentImage(newImage);
-		}
-	};
+    if (sumIdx >= images.length) {
+      setCurrentImage(images[0]);
+    } else if (sumIdx < 0) {
+      setCurrentImage(images[images.length - 1]);
+    } else {
+      setCurrentImage(newImage);
+    }
+  };
 
   const showModalHandler = (e) => {
-		const image = getCurrentImage(e, images);
-		setCurrentImage(image);
+    const image = getCurrentImage(e, images);
+    setCurrentImage(image);
     setShowModal(true);
   }
 
   const closeModalHandler = () => {
-		setCurrentImage(images[0]);
+    setCurrentImage(images[0]);
     setShowModal(false);
   }
 
-	const quantityHandler = (quantity) => {
-		setProductQuantity(prevQuantity => {
-			if (prevQuantity === 0 && quantity < 0) {
-				return prevQuantity;
-			} else {
-				return prevQuantity + quantity;
-			}
-		});
-	}
+  const quantityHandler = (quantity) => {
+    setProductQuantity(prevQuantity => {
+      if (prevQuantity === 0 && quantity < 0) {
+        return prevQuantity;
+      } else {
+        return prevQuantity + quantity;
+      }
+    });
+  }
 
-	const addToCartHandler = () => {
-		if (productQuantity === 0) {
-			return;
-		}
+  const addToCartHandler = () => {
+    if (productQuantity === 0) {
+      return;
+    }
 
-		const totalPrice = productInfo.price * productQuantity;
-		ctx.onAddToCart(
-			productInfo.id, 
-			productInfo.title,
-			productQuantity, 
-			productInfo.price, 
-			totalPrice, 
-			productInfo.image
-		);
+    const totalPrice = productInfo.price * productQuantity;
+    ctx.onAddToCart(
+      productInfo.id,
+      productInfo.title,
+      productQuantity,
+      productInfo.price,
+      totalPrice,
+      productInfo.image
+    );
 
-		setProductQuantity(0);
-	}
+    setProductQuantity(0);
+  }
 
-	return (
-		<Fragment>
-			<div className={classes['product-wrapper']}>
-				<div className={classes['product-images']}>
-					{
-						mobile ? (
-							<div className={classes['images-mobile']}>
-								<Previous 
-									className={classes['prev-mobile']}
-									onClick={() => changeImageHandler(-1)}
-								/>
-								<img className={classes['main-image']} src={currentImage.src} alt={currentImage.id} />
-								<Next 
-									className={classes['next-mobile']}
-									onClick={() => changeImageHandler(1)} 
-								/>
-							</div>
-						) : 
-							<img className={classes['main-image']} src={images[0].src} alt={currentImage.id} />
-					}
-					<ImagesThumbnail 
-						showModal={showModal} 
-						images={images} 
-						onClick={(e) => showModalHandler(e)} 
-					/>
-				</div>
-				<div className={classes['product-description']}>
-					<p className={classes['company-title']}>SNEAKER COMPANY</p>
-					<h1 className={classes['product-title']}>{productInfo.title}</h1>
-					<p>
-						{productInfo.description}
-					</p>
-					<div className={classes['price-wrapper']}>
-						<h2 className={classes['product-price']}>
-							$125.00 <span className={classes.discount}>50%</span>
-						</h2>
-						<p className={classes['price-before']}>$250.00</p>
-					</div>
-					<div className={classes['product-controls']}>
-						<QuantityControls onQuantityChange={quantityHandler}>
-							{productQuantity}
-						</QuantityControls>
-						<Button onClick={addToCartHandler}>
-							<span>
-								<img className={classes['cart-icon']} src={icons.IconCart} alt="cart-icon" />
-							</span>
-							Add To Cart
-						</Button>
-					</div>
-				</div>
-			</div>
-			{
-				(showModal && !mobile) && (
-					<Modal onClose={closeModalHandler}>
-						<img 
-							className={classes['modal-close']} 
-							src={icons.IconClose} 
-							alt="close"
-							onClick={closeModalHandler} 
-						/>
-						<Previous 
-							className={classes.prev}
-							onClick={() => changeImageHandler(-1)} 
-						/>
-						<div className={classes['modal-image']}>
-							<img src={currentImage.src} alt={currentImage.id} />
-						</div>
-						<Next 
-							className={classes.next}
-							onClick={() => changeImageHandler(1)}
-						 />
-						<ImagesThumbnail images={images} onClick={(e) => openImageHandler(e)} />
-					</Modal>
-				)
-			}
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <div className={classes['product-wrapper']}>
+        <div className={classes['product-images']}>
+          {
+            mobile ? (
+              <div className={classes['images-mobile']}>
+                <Previous
+                  className={classes['prev-mobile']}
+                  onClick={() => changeImageHandler(-1)}
+                />
+                <img className={classes['main-image']} src={currentImage.src} alt={currentImage.id} />
+                <Next
+                  className={classes['next-mobile']}
+                  onClick={() => changeImageHandler(1)}
+                />
+              </div>
+            ) :
+              <img className={classes['main-image']} src={images[0].src} alt={currentImage.id} />
+          }
+          <ImagesThumbnail
+            showModal={showModal}
+            images={images}
+            onClick={(e) => showModalHandler(e)}
+          />
+        </div>
+        <div className={classes['product-description']}>
+          <p className={classes['company-title']}>SNEAKER COMPANY</p>
+          <h1 className={classes['product-title']}>{productInfo.title}</h1>
+          <p>
+            {productInfo.description}
+          </p>
+          <div className={classes['price-wrapper']}>
+            <h2 className={classes['product-price']}>
+              $125.00 <span className={classes.discount}>50%</span>
+            </h2>
+            <p className={classes['price-before']}>$250.00</p>
+          </div>
+          <div className={classes['product-controls']}>
+            <QuantityControls onQuantityChange={quantityHandler}>
+              {productQuantity}
+            </QuantityControls>
+            <Button onClick={addToCartHandler}>
+              <span>
+                <img className={classes['cart-icon']} src={icons.IconCart} alt="cart-icon" />
+              </span>
+              Add To Cart
+            </Button>
+          </div>
+        </div>
+      </div>
+      {
+        (showModal && !mobile) && (
+          <Modal onClose={closeModalHandler}>
+            <img
+              className={classes['modal-close']}
+              src={icons.IconClose}
+              alt="close"
+              onClick={closeModalHandler}
+            />
+            <Previous
+              className={classes.prev}
+              onClick={() => changeImageHandler(-1)}
+            />
+            <div className={classes['modal-image']}>
+              <img src={currentImage.src} alt={currentImage.id} />
+            </div>
+            <Next
+              className={classes.next}
+              onClick={() => changeImageHandler(1)}
+            />
+            <ImagesThumbnail images={images} onClick={(e) => openImageHandler(e)} />
+          </Modal>
+        )
+      }
+    </Fragment>
+  );
 }
 
 export default Product;
