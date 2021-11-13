388fafabdb3207674ed905a5e870f314f1d6682f
Eugene Kiryk <kiryk.eugene@gmail.com>
2021-11-13 22:40:36 +0100

    file format

--- src/store/context.js
+++ src/store/context.js
@@ -1,88 +1,88 @@
 import React, { useState, useEffect } from 'react';
 
-const AppContext = React.createContext({ 
-	windowWidth: null, 
-	cartItems: [],
-	cartQuantity: 0,
-	onAddToCart: (id, product, quantity, price, totalPrice, image) => {},
-	onRemoveFromCart: () => {}
+const AppContext = React.createContext({
+  windowWidth: null,
+  cartItems: [],
+  cartQuantity: 0,
+  onAddToCart: (id, product, quantity, price, totalPrice, image) => { },
+  onRemoveFromCart: () => { }
 });
 
 export function AppContextProvider({ children }) {
-	const [windowWidth, setWindowWidth] = useState(null);
-	const [cartItems, setCartItems] = useState([]);
-	const [cartQuantity, setCartQuantity] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartQuantity, setCartQuantity] = useState(0);
 
   const windowResizeHandler = () => {
-		setWindowWidth(window.innerWidth);
-	}
+    setWindowWidth(window.innerWidth);
+  }
 
-	useEffect(() => {
-		setWindowWidth(window.innerWidth);
-	}, []);
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
 
-	useEffect(() => {
-		window.addEventListener('resize', windowResizeHandler);
+  useEffect(() => {
+    window.addEventListener('resize', windowResizeHandler);
 
-		return () => {
-			window.removeEventListener('resize', windowResizeHandler);
-		}
-	}, [windowWidth]);
+    return () => {
+      window.removeEventListener('resize', windowResizeHandler);
+    }
+  }, [windowWidth]);
 
-	useEffect(() => {
-		let updatedQuantity = 0;
-		cartItems.forEach(item => updatedQuantity += item.quantity)
-		setCartQuantity(updatedQuantity);
-	}, [cartItems]);
+  useEffect(() => {
+    let updatedQuantity = 0;
+    cartItems.forEach(item => updatedQuantity += item.quantity)
+    setCartQuantity(updatedQuantity);
+  }, [cartItems]);
 
-	const addToCartHandler = (id, product, quantity, price, priceTotal, image) => {
-		const itemIdx = cartItems.findIndex(item => item.product === product);
+  const addToCartHandler = (id, product, quantity, price, priceTotal, image) => {
+    const itemIdx = cartItems.findIndex(item => item.product === product);
 
-		if (itemIdx === -1) {
-			setCartItems(prevState => {
-				return [
-					...prevState, 
-					{ 
-						id, 
-						product,  
-						quantity, 
-						price, 
-						priceTotal, 
-						image 
-					}];
-			});
-		} else {
-			const newCart = [...cartItems];
-			newCart[itemIdx] = {
-				id,
-				product,
-				quantity: newCart[itemIdx].quantity + quantity,
-				price,
-				priceTotal: newCart[itemIdx].priceTotal + priceTotal,
-				image
-			}
-			setCartItems(newCart);
-		}
-	}
+    if (itemIdx === -1) {
+      setCartItems(prevState => {
+        return [
+          ...prevState,
+          {
+            id,
+            product,
+            quantity,
+            price,
+            priceTotal,
+            image
+          }];
+      });
+    } else {
+      const newCart = [...cartItems];
+      newCart[itemIdx] = {
+        id,
+        product,
+        quantity: newCart[itemIdx].quantity + quantity,
+        price,
+        priceTotal: newCart[itemIdx].priceTotal + priceTotal,
+        image
+      }
+      setCartItems(newCart);
+    }
+  }
 
-	const removeFromCerdHandler = (id) => {
-		const newCart = [...cartItems].filter(item => item.id !== id);
-		setCartItems(newCart);
-	}
+  const removeFromCerdHandler = (id) => {
+    const newCart = [...cartItems].filter(item => item.id !== id);
+    setCartItems(newCart);
+  }
 
   return (
     <AppContext.Provider
-      value={{ 
-				windowWidth: windowWidth,
-				cartItems: cartItems,
-				cartQuantity: cartQuantity,
-				onAddToCart: addToCartHandler,
-				onRemoveFromCart: removeFromCerdHandler 
-			}}
+      value={{
+        windowWidth: windowWidth,
+        cartItems: cartItems,
+        cartQuantity: cartQuantity,
+        onAddToCart: addToCartHandler,
+        onRemoveFromCart: removeFromCerdHandler
+      }}
     >
       {children}
     </AppContext.Provider>
   )
-} 
+}
 
 export default AppContext;
